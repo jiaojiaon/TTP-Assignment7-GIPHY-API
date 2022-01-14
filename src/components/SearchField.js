eb4a1a9d3518612deb8b4509478ea02fcf9fdea5
jiaojiaon <jiaojiaon@gmail.com>
2022-01-14 16:21:32 -0500

    Fixed the gifs display issue

--- src/components/SearchField.js
+++ src/components/SearchField.js
@@ -7,9 +7,8 @@ const SearchField  = (props) => {
         props.setGifs(
             gifData.data
         );
-        console.log(gifData.data)
     }
-   const handleChange = (event) => {
+   const handleRegularSearch = (event) => {
         let name = event.target.name;
         let value = event.target.value;
         props.setGifs({
@@ -17,6 +16,45 @@ const SearchField  = (props) => {
         })
         let searchVar = document.getElementById('search').value
         getGifsName(searchVar)
+    }
+
+    // Random Search:
+    const getRandomGif = async () => {
+        const url = ("http://api.giphy.com/v1/gifs/random?api_key=O4gwEGwV02bdZOZQaD0dJsGiyjPjuM21");
+        const response = await fetch(url);
+        const gifData = await response.json();
+        props.setGifs(
+            [gifData.data]
+        );
+        console.log(gifData.data)
+    }
+    const handleRandomSearch = (event) => {
+        let name = event.target.name;
+        let value = event.target.value;
+        props.setGifs({
+            [name]: value
+        })
+        let searchVar = document.getElementById('search').value
+        getRandomGif(searchVar)
+    }
+    //Trending Search:
+    const getTrendingGif = async () => {
+        const url = ("http://api.giphy.com/v1/gifs/trending?api_key=O4gwEGwV02bdZOZQaD0dJsGiyjPjuM21");
+        const response = await fetch(url);
+        const gifData = await response.json();
+        props.setGifs(
+            gifData.data
+        );
+    }
+
+    const handleTrendingSearch = (event) => {
+        let name = event.target.name;
+        let value = event.target.value;
+        props.setGifs({
+            [name]: value
+        })
+        let searchVar = document.getElementById('search').value
+        getTrendingGif(searchVar)
     }
         return (
             <>
@@ -27,9 +65,9 @@ const SearchField  = (props) => {
                         {/* for search field input and button  */}
                         <div className="submit-container">
                             <input id='search' placeholder="Search Gifs Name"></input>
-                            <button id="searchButton" type='button' onClick={handleChange}>Search</button>
-                            <button id="trendingSearchButton" type='button' onClick={handleChange}>Trending Search</button>
-                            <button id="randomSearchButton" type='button' onClick={handleChange}>Random Search</button>
+                            <button id="searchButton" type='button' onClick={handleRegularSearch}>Search</button>
+                            <button id="trendingSearchButton" type='button' onClick={handleTrendingSearch}>Trending Search</button>
+                            <button id="randomSearchButton" type='button' onClick={handleRandomSearch}>Random Search</button>
 
 
 
